63f75ee66dff269776f41814c39ad190b35c00b2
Ksantox <deniskrumov95@gmail.com>
2022-08-16 16:33:08 +0300

    Submit problem 387 optimized solution

--- 387.FirstUniqueCharacter/index.js
+++ 387.FirstUniqueCharacter/index.js
@@ -1,30 +1,20 @@
 function firstUniqChar(text) {
     const map = {};
     const queue = [];
-
-    for (let i = 0; i < text.length; i++) {
-        if(map[text[i]] !== undefined) {
-            map[text[i]]++;
+    
+    for(let i = 0; i < text.length; i++) {
+        if(map[text[i]] === undefined) {
+            map[text[i]] = i;
+            queue.push(text[i]);
             continue;
         }
-
-        map[text[i]] = 1;
-        queue.push([text[i], i]);
-    }
-
-    let characterIndex = -1;
-
-    for (let i = 0; i < text.length; i++) {
-        if(map[text[i]] === 1) {
-            return i;
-        }
-
-        if(queue.length && queue[0][0] === text[i]) {
+        
+        if(queue[0] === text[i]) {
             queue.shift();
         }
     }
-
-    return characterIndex;
+    
+    return map[queue[0]] || -1;
 }
 
 export default firstUniqChar;
